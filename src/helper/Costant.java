472a77d68fdf0d3c7230d7a6b6300f4dfc903948
Alessandro D'Orazio <me@alessandrodorazio.it>
2019-05-27 12:38:33 +0200

    Removed constant reform

--- src/helper/Costant.java
+++ src/helper/Costant.java
@@ -1,7 +1,7 @@
 package helper;
 
 public class Costant {
-	public final static int reform = 0; //1 = only heavy vehicles, 2 = all vehicles
+
 	public final static int EURO_MAX = 6;
 	public final static float IVA = .22f; //22%
 	
