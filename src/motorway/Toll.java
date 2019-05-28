edbfdd158c413f9868ffa1fae10289dbd60a8681
Alessandro D'Orazio <me@alessandrodorazio.it>
2019-05-28 16:47:59 +0200

    updated toll

--- src/motorway/Toll.java
+++ src/motorway/Toll.java
@@ -1,11 +1,43 @@
+/**
+ * 
+ */
 package motorway;
 
+import helper.Helper;
+import helper.Costant;
 import tollbooth.Tollbooth;
 import vehicle.Vehicle;
 
-interface Toll {
+/**
+ * @author alessandrodorazio
+ *
+ */
+public class Toll {
 	
-	float tollCalc(Tollbooth in, Tollbooth out, Vehicle v);
-	int routeCalc(Tollbooth in, Tollbooth out);
+	int id;
+	String vehicle;
+	float cost;
+	Tollbooth in, out;
+
+	public Toll(int id, String vehicle, float cost, Tollbooth in, Tollbooth out) {
+		this.id = id;
+		this.vehicle = vehicle;
+		this.cost = cost;
+		this.in = in;
+		this.out = out;
+		//send data
+	}
+
+	public static float tollCalc(Tollbooth in, Tollbooth out, Vehicle v) {
+		float km = routeCalc(in, out);
+        if(km == 0) throw new Error("I due caselli coincidono"); //throw exception
+        float cost = km * Motorway.getInstance().getUnitRateSingle(v) * (1 + Costant.IVA); //calcolo del pedaggio
+        return Helper.round(cost); //arrotondiamo tramite la classe Helper
+	}
+
+	public static int routeCalc(Tollbooth in, Tollbooth out) {
+		return (in.getKm() > out.getKm())?(in.getKm() - out.getKm()):(out.getKm() - in.getKm());
+	}
+
 	
 }
